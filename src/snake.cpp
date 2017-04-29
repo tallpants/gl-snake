26de3ac88387a0931203e0888c49d2f24c455f4e
Adithya <adithyakreddy6@gmail.com>
2017-04-29 11:15:42 +0530

    This is a mess

--- src/snake.cpp
+++ src/snake.cpp
@@ -3,6 +3,41 @@
 #include <deque>
 #include <unistd.h>
 
+// Snake directions
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+
+char title[] = "Snake";
+float map_half_length = 30.0;
+
+// The current direction of the snake is stored here.
+// Initial direction when the game starts is DOWN.
+int direction = DOWN;
+
+// For glutTimerFunc()
+// The registered callback function will be triggered in
+// intervals of the milliseconds specified here.
+int timer = 100;
+
+// Did the player change the direction of the snake?
+bool changed_direction = false;
+
+// Maintains the coordinates of the body of the snake
+std::deque< std::deque<float> > part_coords;
+
+// Is there food on the map right now?
+bool food_available = false;
+
+// X, Y coordinates of the food on the map
+int food_coords[2];
+
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
+}
+
 int main(int argc, char **argv) {
 
   glutInit(&argc, argv);
@@ -10,8 +45,10 @@ int main(int argc, char **argv) {
   glutInitWindowSize(600, 600);
   glutCreateWindow("Snake");
 
+  glutDisplayFunc(display);
+
   glMatrixMode(GL_PROJECTION);
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  
+  glClearColor(1.0, 1.0, 1.0, 1.0);
 
   glutMainLoop();
 
