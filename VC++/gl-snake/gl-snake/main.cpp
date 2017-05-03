6659f33ae8712a36863067e1fcd0e0cd4527c464
Adithya <adithyakreddy6@gmail.com>
2017-05-03 16:32:29 +0530

    Fixed bug with inserting when nodes were already present

--- VC++/gl-snake/gl-snake/main.cpp
+++ VC++/gl-snake/gl-snake/main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 
-typedef struct coordinate
-{
+struct coordinate {
 	int x;
 	int y;
 };
 
-class Snake
-{
-	typedef struct node
-	{
+class Snake {
+	struct node {
 		coordinate data;
-		node *next;
-		node *prev;
+		node* next;
+		node* prev;
 	};
 
 	node* head;
@@ -21,18 +18,14 @@ class Snake
 public:
 	int size;
 
-	Snake()
-	{
+	Snake() {
 		size = 0;
 		head = nullptr;
 		tail = nullptr;
 	}
 
-	void push_front(coordinate c)
-	{
-		// Is the structure empty?
-		if (size == 0)
-		{
+	void push_front(coordinate c) {
+		if (size == 0) {
 			head = new node;
 			head->data.x = c.x;
 			head->data.y = c.y;
@@ -41,12 +34,11 @@ public:
 			tail = head;
 			size += 1;
 		}
-
-		else
-		{
+		else {
 			node* temp = new node;
 			temp->data.x = c.x;
 			temp->data.y = c.y;
+			temp->next = head;
 			temp->prev = nullptr;
 			head->prev = temp;
 			head = temp;
@@ -54,23 +46,35 @@ public:
 		}
 	}
 
-	//coordinate pop_back();
-
-	coordinate operator [] (int index) {
+	coordinate operator[] (int index) {
 		if (index == 0) {
 			coordinate c = { head->data.x, head->data.y };
 			return c;
 		}
+		
+		int i = 0;
+		node* temp = head;
+		while (i != index) {
+			temp = temp->next;
+			i += 1;
+		}
+
+		coordinate c = { temp->data.x, temp->data.y };
+		return c;
 	}
 };
 
-void main()
-{
+void main() {
 	Snake s;
 	coordinate a = { 10, 20 };
 	coordinate b = { 30, 40 };
+	coordinate c = { 50, 60 };
+
 	s.push_front(a);
 	s.push_front(b);
-	std::cout << s[0].x << " " << s[0].y << std::endl;
+	s.push_front(c);
+
+	std::cout << s[1].x << " " << s[1].y << std::endl;
+	
 	std::cin.get();
 }
