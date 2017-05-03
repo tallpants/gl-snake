8918e0abc9533594924c0bdc9981577163273741
Adithya <adithyakreddy6@gmail.com>
2017-05-03 16:59:21 +0530

    Cleaned up and added comments

--- VC++/gl-snake/gl-snake/main.cpp
+++ VC++/gl-snake/gl-snake/main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-
+// Storage container for coordinates of the blocks forming the snake's
+// body and the food.
 struct coordinate {
 	int x;
 	int y;
 };
 
+// Partial deque implementation that encapsulates the snake's body
 class Snake {
 	struct node {
 		coordinate data;
@@ -16,15 +17,19 @@ class Snake {
 	node* tail;
 
 public:
+	// Current size of the snake's body
 	int size;
 
+	// Default constructor for empty snake
 	Snake() {
 		size = 0;
 		head = nullptr;
 		tail = nullptr;
 	}
 
+	// Push a block onto the head of the snake
 	void push_front(coordinate c) {
+		// Is this the first block of the snake?
 		if (size == 0) {
 			head = new node;
 			head->data.x = c.x;
@@ -33,8 +38,7 @@ public:
 			head->prev = nullptr;
 			tail = head;
 			size++;
-		}
-		else {
+		} else {
 			node* temp = new node;
 			temp->data.x = c.x;
 			temp->data.y = c.y;
@@ -46,20 +50,24 @@ public:
 		}
 	}
 
+	// Pop the rearmost block off of the snake's body
 	void pop_back() {
 		tail = tail->prev;
 		tail->next = nullptr;
 		size--;
 	}
 
+	// Access the blocks of the snake using the subscript operator
 	coordinate operator[] (int index) {
+		// The first block of the snake is pointed to by head
 		if (index == 0) {
 			coordinate c = { head->data.x, head->data.y };
 			return c;
 		}
-		
-		int i = 0;
+
+		// Traverse the structure till you get to the right node
 		node* temp = head;
+		int i = 0;
 		while (i != index) {
 			temp = temp->next;
 			i++;
@@ -69,27 +77,3 @@ public:
 		return c;
 	}
 };
-
-void main() {
-	Snake s;
-	coordinate a = { 10, 20 };
-	coordinate b = { 30, 40 };
-	coordinate c = { 50, 60 };
-
-	s.push_front(a);
-	s.push_front(b);
-	s.push_front(c);
-
-	for (int i = 0; i < s.size; i++) {
-		std::cout << s[i].x << " " << s[i].y << std::endl;
-	}
-	
-	std::cout << "Popping once" << std::endl << std::endl;
-	s.pop_back();
-
-	for (int i = 0; i < s.size; i++) {
-		std::cout << s[i].x << " " << s[i].y << std::endl;
-	}
-
-	std::cin.get();
-}
